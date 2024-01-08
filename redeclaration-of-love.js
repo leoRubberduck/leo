e935804085ae22056238e6f112bcf90a6201fa8a
leokelly <leokelly333@outlook.com>
2024-01-08 17:38:12 +0000

    first commit

--- redeclaration-of-love.js
+++ redeclaration-of-love.js
@@ -1,8 +1,6 @@
 
 let love = 'You used to love me !!';
 
-console.log('First value of love:', love);
-
 love = 'I still love you !!'; // Reassigning the value
 
 console.log('Reassigned value of love:', love);
