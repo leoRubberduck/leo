510881389d30770065bc038d8832772d6449c4f8
leokelly <leokelly333@outlook.com>
2024-01-08 15:17:08 +0000

    first commit2

---
+++ anything-to-declare.js
@@ -0,0 +1,9 @@
+// Declare variables
+let age = 30;
+let name = 'Leo'; 
+let secureLuggage = false;
+
+// Output the variables using console.log
+console.log('Age:', age);
+console.log('Name:', name);
+console.log('Secure Luggage:', secureLuggage);
