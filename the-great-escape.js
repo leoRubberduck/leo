f6c2ee083e3bfd0fc1f1a2ee766870f3ab958a47
leokelly <leokelly333@outlook.com>
2024-01-08 15:53:17 +0000

    first commits2

---
+++ the-great-escape.js
@@ -0,0 +1,9 @@
+//escape with ""
+const escapeFromDelimiters = "`\"\'";
+
+// Escape with \\
+const escapeTheEscape = "\\";
+
+// Testing the sequence
+console.log("Escaped Delimiters:", escapeFromDelimiters);
+console.log("Escaped Escape Character:", escapeTheEscape);
