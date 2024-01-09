5bdac58954a232a0391d0ab0a885bb2c7da5dc6e
leokelly <leokelly333@outlook.com>
2024-01-09 12:18:53 +0000

    first commits2

--- access.js
+++ access.js
@@ -1,12 +1,9 @@
-let human = {
+human = {
     name: 'Leo',
     age: 30,
     secureLuggage: true
   };
-
-
-
-
-let name = human.name;
+  
+  let name = human.name;
 let age = human.age;
 let secureLuggage = human.secureLuggage;
