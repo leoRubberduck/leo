8f14953c6bf2c05f03c6735564c63d01d917be70
leokelly <leokelly333@outlook.com>
2024-01-09 14:26:38 +0000

    first commits2

--- nested-objects.js
+++ nested-objects.js
@@ -1,11 +1,12 @@
 let address = {
-
-city: 'String',
-postcode: 'Number',
-street: {
-
-    name: String,
-    number: Number,
-}
-}
+    city: String,         
+    postcode: Number,     
+    street: {             
+      name: String,      
+      number: Number   
+    }
+  };
+  
+  console.log(address);
+  
 
