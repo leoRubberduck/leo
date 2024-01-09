bda225c425b6f53d9b1871fd6403ebf49c3b3357
leokelly <leokelly333@outlook.com>
2024-01-09 13:10:34 +0000

    first commits2

--- alter-ego.js
+++ alter-ego.js
@@ -1,3 +1,5 @@
 alterEqo.self = 'altered';
 alterEgo.fullName = alterEgo.firstName + ' ' + alterEgo.lastName;
-alterEgo.points = points + 10;
+alterEgo.points = points += 10;
+
+console.log(alterEgo);
