48f0f4883382e2262a6ac85d4b4b5a30dca57557
leokelly <leokelly333@outlook.com>
2024-01-10 16:47:42 +0000

    first commits2

--- keep.js
+++ keep.js
@@ -1,3 +1,3 @@
-let keepFirst = alphabet.slice(6);
-let keepLast = alphabet.slice(7);
-let keepFirstLast = alphabet.slice(3,4);
+let keepFirst = alphabet.slice(0,6);
+let keepLast = alphabet.slice(-7);
+let keepFirstLast = alphabet.slice(0,3) + alphabet.slice(-4);
