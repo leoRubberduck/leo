d05eacd1f795eb6ad71f050eb271448403fa243a
leokelly <leokelly333@outlook.com>
2024-01-11 21:54:57 +0000

    first commit

--- griswold-the-blacksmith.js
+++ griswold-the-blacksmith.js
@@ -5,17 +5,17 @@ const bootsPrice = 44;
 const swordPrice = 299.99;
 
 if (playerCoins >= arrowPrice) {
-    purchasableGoods.push("Arrows");
-  }
-  
-  if (playerCoins >= bootsPrice) {
-    purchasableGoods.push("Boots");
-  }
-  
-  if (playerCoins >= swordPrice) {
-    purchasableGoods.push("Sword (Limited Offer)");
-  }
-  
+  purchasableGoods.push({ item: "Arrows", price: arrowPrice });
+}
 
+if (playerCoins >= bootsPrice) {
+  purchasableGoods.push({ item: "Boots", price: bootsPrice });
+}
+
+if (playerCoins >= swordPrice) {
+  purchasableGoods.push({ item: "Sword (Limited Offer)", price: swordPrice });
+}
+
+purchasableGoods.sort((a, b) => a.price - b.price);
 
 
