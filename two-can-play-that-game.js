54cbb3d243d7bc9583af88be5dc917d8985491fb
leokelly <leokelly333@outlook.com>
2024-01-12 15:07:27 +0000

    first commit

--- two-can-play-that-game.js
+++ two-can-play-that-game.js
@@ -1,4 +1,7 @@
 let duos = (batman, Robin) => {
     console.log(`${batman} and ${Robin} !`);
   }
-  
+
+  let duosWork = (batman, robin, action) => {
+    console.log(`${batman} and ${robin} ${protect Gotham} !`);
+  }
