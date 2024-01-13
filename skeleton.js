b328059d0a62e0288c0a1b244a49d141698dd31a
leokelly <leokelly333@outlook.com>
2024-01-13 20:25:05 +0000

    first commit

--- skeleton.js
+++ skeleton.js
@@ -1,18 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Your Entity's Name</title>
 </head>
+
 <body>
+  <section>
+    face
+  </section>
 
-    <div>face</div>
+  <section>
+    upper-body
+  </section>
 
-    <div>upper-body</div>
-
-    <div>lower-body</div>
-
-    
+  <section>
+    lower-body
+  </section>
 </body>
+
 </html>
