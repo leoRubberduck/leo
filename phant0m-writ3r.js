704fa2ceb1f9ef9d7689940d8384099ec5fa841a
leokelly <leokelly333@outlook.com>
2024-01-14 17:35:09 +0000

    first commit

--- phant0m-writ3r.js
+++ phant0m-writ3r.js
@@ -1,3 +1,11 @@
-if (!(word.toLowerCase().startsWith('al') && word.toLowerCase().endsWith('ion'))) {
+if (!(startsWithAl(word) && endsWithIon(word))) {
     console.log(word);
-}
+  }
+  
+  function startsWithAl(word) {
+    return word.toLowerCase().startsWith('al');
+  }
+  
+  function endsWithIon(word) {
+    return word.toLowerCase().endsWith('ion');
+  }
