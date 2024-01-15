562027263b737525dbb893b7865c30d5d4e5377c
leokelly <leokelly333@outlook.com>
2024-01-15 18:58:53 +0000

    myfirst

--- extremes.js
+++ extremes.js
@@ -1,22 +1,20 @@
 function extreme(arr) {
     if (arr.length === 0) {
-      // Handle empty array case
+
       return null;
     }
   
     let smallest = arr[0];
-    let bigest = arr[0];
+    let biggest = arr[0];
   
     for (let i = 1; i < arr.length; i++) {
       if (arr[i] < smallest) {
         smallest = arr[i];
       }
-      if (arr[i] > bigest) {
-        bigest = arr[i];
+      if (arr[i] > biggest) {
+        biggest = arr[i];
       }
     }
   
-    return { bigest, smallest };
-  }
-  
-  
+    return { biggest, smallest };
+  };
