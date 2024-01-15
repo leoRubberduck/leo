262aea4f14fac8922c2f8d661d1a6a730dccdb18
leokelly <leokelly333@outlook.com>
2024-01-15 17:22:20 +0000

    myfirst

--- to-infinity-and-beyond.js
+++ to-infinity-and-beyond.js
@@ -1,7 +1,7 @@
 
   
   if (candidate.physicalAptitudes < 80) {
-    console.log('Stay on earth');
+    console.log('earth');
   } else if (candidate.noFamily) {
     console.log('Go to Mars');
   } else {
