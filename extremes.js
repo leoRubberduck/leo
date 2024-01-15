0f86a9aa6a4a9f90edeaa9612d9bc93510e174f1
leokelly <leokelly333@outlook.com>
2024-01-15 16:33:06 +0000

    commit2

--- extremes.js
+++ extremes.js
@@ -1,18 +1,18 @@
 function extreme(numbers) {
     if (numbers.length === 0) {
-        
+
       return null;
     }
   
     let smallest = numbers[0];
-    let biggest = numbers[0];
+    let bigest = numbers[0];
   
     for (let i = 1; i < numbers.length; i++) {
       if (numbers[i] < smallest) {
         smallest = numbers[i];
       }
       if (numbers[i] > biggest) {
-        biggest = numbers[i];
+        bigest = numbers[i];
       }
     }
   
