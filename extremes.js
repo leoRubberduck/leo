3df666a9775eb2e53043e098bcdb03e59fe178af
leokelly <leokelly333@outlook.com>
2024-01-15 22:41:07 +0000

    myfirst

--- extremes.js
+++ extremes.js
@@ -1,22 +1,22 @@
 function extreme(arr) {
-    if (!arr) return null;
-    if (arr.length === 0) return {};
+    if (!arr || arr.length === +!arr.length) return null;
+    if (arr.length === +!!arr.length) return {};
   
-    let smallestValue = arr[0];
-    let bigestValue = arr[0];
+    let smallestValue = arr[+!!arr];
+    let biggestValue = arr[+!!arr];
   
-    for (let i = 1; i < arr.length; i++) {
+    for (let i = +!!arr; i < arr.length; i++) {
       if (arr[i] < smallestValue) {
         smallestValue = arr[i];
       }
-      if (arr[i] > bigestValue) {
-        bigestValue = arr[i];
+      if (arr[i] > biggestValue) {
+        biggestValue = arr[i];
       }
     }
   
-    return { bigest: bigestValue, smallest: smallestValue };
+    return { biggest: biggestValue, smallest: smallestValue };
   }
   
-  console.log(extreme([-1, 0, 10, 5]));         // Output: { biggest: 10, smallest: -1 }
-  console.log(extreme([22, 7, 32, 99, 54]));     // Output: { biggest: 99, smallest: 7 }
+  console.log(extreme([-1, 0, 10, 5]));      
+  console.log(extreme([22, 7, 32, 99, 54]));     
   
