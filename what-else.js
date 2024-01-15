c47c85bf88a85eae31f852cf65444db4f0a464f2
leokelly <leokelly333@outlook.com>
2024-01-15 17:18:04 +0000

    myfirst

---
+++ what-else.js
@@ -0,0 +1,7 @@
+const vips = ['John Doe', 'Jane Doe', 'George Clooney', 'Alice'];
+
+if (vips.includes('George Clooney')) {
+  console.log('What else ?');
+} else {
+  console.log('The only failure is not to try.');
+}
