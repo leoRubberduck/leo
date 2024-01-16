ab93f1050bef1309398bdb17acb215fd981f054a
leokelly <leokelly333@outlook.com>
2024-01-16 00:53:12 +0000

    hello

--- extremes.js
+++ extremes.js
@@ -17,6 +17,3 @@ function extreme(arr) {
 
   return { bigest, smallest };
 }
-
-console.log(extreme([-1, 0, 10, 5])); // -> { bigest: 10, smallest: -1 }
-console.log(extreme([22, 7, 32, 99, 54])); // -> { bigest: 99, smallest: 7 }
