103fc95bff95ecfae7d0747fddc1f3eff8911525
leokelly <leokelly333@outlook.com>
2024-01-17 01:36:23 +0000

    hello

--- jaden-case.js
+++ jaden-case.js
@@ -1,7 +1,7 @@
 function toJadenCase(sentence) {
     return sentence
       .split(' ')
-      .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
       .join(' ');
   }
   
