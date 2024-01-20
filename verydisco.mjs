7166a022aafe287ccd02551cdde97d17261ae60c
leokelly <leokelly333@outlook.com>
2024-01-20 13:28:22 +0000

    mycommitt

--- verydisco.mjs
+++ verydisco.mjs
@@ -1,18 +1,14 @@
 import { argv } from 'process';
-
 const originalArgument = argv[2];
-
 function makeVeryDisco(word) {
   const middle = Math.ceil(word.length / 2);
   const firstChunk = word.slice(0, middle);
   const secondChunk = word.slice(middle);
   return secondChunk + firstChunk;
 }
-
 function makeVeryDiscoSentence(sentence) {
   return sentence.split(/\s+/).map(makeVeryDisco).join(' ');
 }
-
 if (!originalArgument) {
   console.log('Please provide an argument');
 } else if (typeof originalArgument !== 'string') {
